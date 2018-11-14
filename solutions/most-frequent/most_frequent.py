785ce7c9e484a1667c7a49e772a9b3faf6d36ad2
Christopher Auglend <chris.auglend@gmail.com>
2018-11-14 19:26:53 +0100

    Added solution to most frequent words and most frequent character using mapping, filter and Counter.

---
+++ solutions/most-frequent/most_frequent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from collections import Counter
+
+
+def strip_special_chars(string):
+    return "".join(char for char in string if char.isalpha())
+
+
+def char_counter(string, number):
+    chars = filter(str.isalpha, string)
+    return Counter(chars).most_common(number)
+
+
+def word_counter(string, number):
+    words = map(strip_special_chars, string.split())
+    return Counter(words).most_common(number)
+
+
+if __name__ == "__main__":
+    file_name = "Dracula"
+    with open(file_name) as page:
+        lines = page.read().lower()
+    print("# Most frequent words:", word_counter(lines, 5))
+    print("# Most frequent chars:", char_counter(lines, 5))
+
+    

8bdf69517c13e3ea34634deaf2f5333e9c9110c7
Christopher Auglend <chris.auglend@gmail.com>
2018-11-14 21:52:41 +0100

    Added three different solutions to reversing a string.

---
+++ solutions/1/reverse_string.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+# Solution 1
+def reverse_string(string):
+    return string[::-1]
+
+# Solution 2
+def reverse_string_2(string):
+    return "".join(reversed(string))
+
+
+# Solution 3
+def reverse_string_3(string):
+    output = ""
+    for char in string:
+        output = char + output
+    return output
+
+
+if __name__ == "__main__":
+    string = "Hello world!"
+    print(reverse_string(string))
+    print(reverse_string_2(string))
+    print(reverse_string_3(string))

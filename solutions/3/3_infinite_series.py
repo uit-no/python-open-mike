2d749584f96831d40739e206c12f3ff1a2a163cc
Radovan Bast <bast@users.noreply.github.com>
2018-11-26 20:30:40 +0100

    infinite series using reversed

--- solutions/3/3_infinite_series.py
+++ solutions/3/3_infinite_series.py
@@ -1,6 +1,5 @@
 def infinite_series1():
     numbers = [1, 2, 3, 4, 5]
-
     while True:
         for i in numbers:
             yield i
@@ -9,7 +8,6 @@ def infinite_series1():
 
 def infinite_series2():
     numbers = [1, 2, 3, 4, 5]
-
     while True:
         for i in numbers:
             yield i
@@ -17,8 +15,18 @@ def infinite_series2():
             yield i
 
 
+def infinite_series3(n):
+    numbers = range(1, n+1)
+    while True:
+        for i in numbers:
+            yield i
+        for i in reversed(numbers):
+            yield i
+
+
 my_series1 = infinite_series1()
 my_series2 = infinite_series2()
+my_series3 = infinite_series3(5)
 
-for _ in range(18):
-    print(next(my_series1), next(my_series2))
+for _ in range(20):
+    print(next(my_series1), next(my_series2), next(my_series3))

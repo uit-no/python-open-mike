fa9bf8bf23ec0987803c56517f3ad1122be80ba2
Radovan Bast <bast@users.noreply.github.com>
2018-11-30 11:29:58 +0100

    use better variable names; closes #7

--- solutions/3/2_clock_itertools.py
+++ solutions/3/2_clock_itertools.py
@@ -2,14 +2,14 @@ from itertools import cycle
 
 
 def clock_generator():
-    m = cycle(['am', 'pm'])
-    _m = next(m)
-    t = cycle([12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    periods = cycle(['am', 'pm'])
+    hours = cycle([12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    period = next(periods)
     while True:
-        _t = next(t)
-        yield f'{_t} {_m}'
-        if _t == 11:
-            _m = next(m)
+        hour = next(hours)
+        yield f'{hour} {period}'
+        if hour == 11:
+            period = next(periods)
 
 
 my_clock = clock_generator()
